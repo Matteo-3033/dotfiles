495a895e2ed82983818e2dd88a43715c9d5133ad
Matteo-3033 <matteo.manzoni2001@gmail.com>
2024-08-13 23:11:08 -0700

    removed dot_config

--- dot_config/nvim/lua/executable_vim-options.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=3")
-vim.cmd("set number")
-vim.api.nvim_set_option("clipboard", "unnamed")
-vim.g.mapleader = " "
-vim.g.python3_host_prog = "/usr/bin/python"
--- dot_config/polybar/scripts/executable_restartPolybar.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-pkill -9 polybar > /dev/null
-polybar &
