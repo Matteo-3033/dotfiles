e23fa0f939d988ed0416b5c9b35691c033f5be80
Matteo-3033 <matteo.manzoni2001@gmail.com>
2024-10-20 18:36:29 +0200

    updated deprecated tsserver

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,49 +1,49 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		{
-			"williamboman/mason-lspconfig.nvim",
-			opts = { automatic_installation = true },
-		},
-	},
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+        {
+            "williamboman/mason-lspconfig.nvim",
+            opts = { automatic_installation = true },
+        },
+    },
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		local lspconfig = require("lspconfig")
-		lspconfig.tsserver.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.jsonls.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			settings = {
-				python = {
-					pythonPath = "~/.local/share/nvim/python/bin/python",
-				},
-			},
-		})
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-		})
+        local lspconfig = require("lspconfig")
+        lspconfig.ts_ls.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.jsonls.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.pyright.setup({
+            capabilities = capabilities,
+            settings = {
+                python = {
+                    pythonPath = "~/.local/share/nvim/python/bin/python",
+                },
+            },
+        })
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.clangd.setup({
+            capabilities = capabilities,
+        })
 
-		-- get info
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		-- go to definition
-		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-		-- go to references
-		--vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-		vim.keymap.set("n", "<leader>gr", function()
-			require("telescope.builtin").lsp_references({})
-		end, {})
-		-- rename symbol
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		-- get code actions
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-	end,
+        -- get info
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        -- go to definition
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        -- go to references
+        --vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>gr", function()
+            require("telescope.builtin").lsp_references({})
+        end, {})
+        -- rename symbol
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+        -- get code actions
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
 }
