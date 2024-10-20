103378ec1a7e98863dab3e8a11addf9fd50fc8ac
Matteo-3033 <matteo.manzoni2001@gmail.com>
2024-10-20 18:37:02 +0200

    added rust support

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			ensure_installed = {
-				"lua",
-				"javascript",
-				"typescript",
-				"python",
-				"c",
-				"cpp",
-			},
-			highlight = { enable = true, disable = { "latex" } },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+
+        config.setup({
+            auto_install = true,
+            ensure_installed = {
+                "lua",
+                "javascript",
+                "typescript",
+                "python",
+                "c",
+                "cpp",
+                "rust",
+                "toml",
+                "json",
+            },
+            highlight = { enable = true, disable = { "latex" } },
+            indent = { enable = true },
+            rainbow = {
+                enable = true,
+                extended_mode = true,
+                max_file_lines = nil,
+            },
+        })
+
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+        vim.wo.foldlevel = 99
+    end,
 }
