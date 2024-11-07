0390b06c57bd257ba87f93f1ec335e8d46495f47
Matteo-3033 <matteo.manzoni2001@gmail.com>
2024-11-07 09:55:01 +0100

    changed terminal shortcut in nvim

--- .config/nvim/lua/plugins/floaterm.lua
+++ .config/nvim/lua/plugins/floaterm.lua
@@ -1,7 +1,7 @@
 return {
 	"voldikss/vim-floaterm",
 	config = function()
-		vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR>", { noremap = true, silent = true })
-		vim.keymap.set("t", "<leader>t", "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>tt", ":FloatermToggle<CR>", { noremap = true, silent = true })
+		vim.keymap.set("t", "<leader>tt", "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true, silent = true })
 	end,
 }
