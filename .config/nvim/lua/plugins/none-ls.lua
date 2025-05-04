5a3d3cc1108c5776225fe5e406dd2420d3a765cf
Matteo Manzoni <matteo.manzoni2001@gmail.com>
2025-05-04 20:59:20 +0200

    prettier indentation size

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,57 +1,58 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- formatters
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = {
-						"javascript",
-						"typescript",
-						"json",
-						"yaml",
-						"html",
-						"css",
-						"markdown",
-						"json",
-						"javascriptreact",
-					},
-				}),
-				-- diagnostics
-				null_ls.builtins.diagnostics.mypy.with({
-					extra_args = function()
-						local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-						return {
-							"--python-executable",
-							virtual .. "/bin/python",
-							"--strict-equality",
-							"--strict",
-							"--extra-checks",
-							"--install-types",
-						}
-					end,
-				}),
-			},
-			-- auto-formatting on save
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end
-			end,
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- formatters
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.prettier.with({
+                    extra_args = { "--tab-width", "4" },
+                    filetypes = {
+                        "javascript",
+                        "typescript",
+                        "json",
+                        "yaml",
+                        "html",
+                        "css",
+                        "markdown",
+                        "json",
+                        "javascriptreact",
+                    },
+                }),
+                -- diagnostics
+                null_ls.builtins.diagnostics.mypy.with({
+                    extra_args = function()
+                        local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+                        return {
+                            "--python-executable",
+                            virtual .. "/bin/python",
+                            "--strict-equality",
+                            "--strict",
+                            "--extra-checks",
+                            "--install-types",
+                        }
+                    end,
+                }),
+            },
+            -- auto-formatting on save
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format()
+                        end,
+                    })
+                end
+            end,
+        })
 
-		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+    end,
 }
