a721563b93eb9d6e4dd3eb7c3cc170d46fc4e3cd
Matteo Manzoni <matteo.manzoni@unimi.it>
2025-07-29 08:45:19 +0200

    added snippets

---
+++ .config/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,19 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "mlaursen/vim-react-snippets",
+    },
+    opts = function()
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        require("vim-react-snippets").lazy_load()
+
+        -- if you do not want to wrap all props in `Readonly<T>`
+        local config = require("vim-react-snippets.config")
+        config.readonly_props = false
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local compare = cmp.config.compare
+    end,
+}
