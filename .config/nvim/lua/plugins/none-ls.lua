ad05c8cc3a29348421a92436a86c167dda579641
Matteo Manzoni <matteo.manzoni@unimi.it>
2025-07-29 08:45:07 +0200

    added typescript react support

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -21,6 +21,7 @@ return {
                         "markdown",
                         "json",
                         "javascriptreact",
+                        "typescriptreact",
                     },
                 }),
                 -- diagnostics
