4f13da3a274389227700cbe1d0597a46da3fa1c5
Matteo Manzoni <matteo.manzoni@unimi.it>
2025-08-21 15:18:03 +0200

    added debugging package

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,79 +1,88 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"tpope/vim-fugitive",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			require("dapui").setup()
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "tpope/vim-fugitive",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+            dapui.setup()
 
-			local dap, dapui = require("dap"), require("dapui")
+            require("nvim-dap-virtual-text").setup({
+                only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+                all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+                clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			-- dap.listeners.before.event_terminated.dapui_config = function()
-			--     dapui.close()
-			-- end
-			-- dap.listeners.before.event_exited.dapui_config = function()
-			--     dapui.close()
-			-- end
+                -- experimental features:
+                all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            })
 
-			vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
-			vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-			vim.keymap.set("n", "<Leader>dt", ":DapTerminate<CR>")
-			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-			vim.api.nvim_set_keymap(
-				"n",
-				"<Leader>dd",
-				':lua require("dapui").close()<CR>',
-				{ noremap = true, silent = true }
-			)
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
-		config = function()
-			local dap = require("dap")
-			dap.configurations.python = {
-				{
-					type = "python",
-					request = "launch",
-					name = "Launch file",
-					program = "${file}",
-					justMyCode = false,
-					stopOnEntry = true,
-					console = "integratedTerminal",
-				},
-				{
-					type = "python",
-					request = "launch",
-					name = "Launch file with arguments",
-					program = "${file}",
-					justMyCode = false,
-					args = function()
-						local args_string = vim.fn.input("Arguments: ")
-						return vim.split(args_string, " ")
-					end,
-					stopOnEntry = true,
-					console = "integratedTerminal",
-				},
-			}
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            -- dap.listeners.before.event_terminated.dapui_config = function()
+            --     dapui.close()
+            -- end
+            -- dap.listeners.before.event_exited.dapui_config = function()
+            --     dapui.close()
+            -- end
 
-			local venv = "~/.local/share/nvim/python/bin/python"
+            vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+            vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+            vim.keymap.set("n", "<Leader>dt", ":DapTerminate<CR>")
+            vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+            vim.api.nvim_set_keymap(
+                "n",
+                "<Leader>dd",
+                ':lua require("dapui").close()<CR>',
+                { noremap = true, silent = true }
+            )
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function()
+            local dap = require("dap")
+            dap.configurations.python = {
+                {
+                    type = "python",
+                    request = "launch",
+                    name = "Launch file",
+                    program = "${file}",
+                    justMyCode = false,
+                    stopOnEntry = true,
+                    console = "integratedTerminal",
+                },
+                {
+                    type = "python",
+                    request = "launch",
+                    name = "Launch file with arguments",
+                    program = "${file}",
+                    justMyCode = false,
+                    args = function()
+                        local args_string = vim.fn.input("Arguments: ")
+                        return vim.split(args_string, " ")
+                    end,
+                    stopOnEntry = true,
+                    console = "integratedTerminal",
+                },
+            }
 
-			require("dap-python").setup(venv, {
-				include_configs = false,
-			})
-		end,
-	},
+            local venv = "~/.local/share/nvim/python/bin/python"
+
+            require("dap-python").setup(venv, {
+                include_configs = false,
+            })
+        end,
+    },
 }
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,38 +1,38 @@
 return {
-	"williamboman/mason.nvim",
-	cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-	config = function()
-		local ensure_installed = {
-			"prettier",
-			"stylua",
-			"black",
-			"isort",
-			"clang-format",
-			"ruff",
-			"mypy",
-			"flake8",
-			"clangd",
-			"codelldb",
-			"css-lsp",
-			"debugpy",
-			"eslint-lsp",
-			"html-lsp",
-			"json-lsp",
-			"lua-language-server",
-			"markuplint",
-			"pyright",
-			"stylelint",
-			"typescript-language-server",
-			"rust-analyzer",
-		}
-		require("mason").setup({
-			ensure_installed = ensure_installed,
-		})
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+    config = function()
+        local ensure_installed = {
+            "prettier",
+            "stylua",
+            "black",
+            "isort",
+            "clang-format",
+            "ruff",
+            "mypy",
+            "flake8",
+            "clangd",
+            "codelldb",
+            "css-lsp",
+            "debugpy",
+            "eslint-lsp",
+            "html-lsp",
+            "json-lsp",
+            "lua-language-server",
+            "markuplint",
+            "pyright",
+            "stylelint",
+            "typescript-language-server",
+            "rust-analyzer",
+        }
+        require("mason").setup({
+            ensure_installed = ensure_installed,
+        })
 
-		vim.api.nvim_create_user_command("MasonInstallAll", function()
-			vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-		end, {})
+        vim.api.nvim_create_user_command("MasonInstallAll", function()
+            vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+        end, {})
 
-		vim.g.mason_binaries_list = ensure_installed
-	end,
+        vim.g.mason_binaries_list = ensure_installed
+    end,
 }
